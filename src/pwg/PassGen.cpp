054dbe0bf898b34db2ca6981cc6e5ec49ba91fff
Kirill Sharapov <kirill.sharapov2018@yandex.ru>
2021-06-11 16:02:37 +0700

    add options -l

--- src/pwg/PassGen.cpp
+++ src/pwg/PassGen.cpp
@@ -6,17 +6,36 @@
 int main(int argc, char* argv[])
 {
     Password password;
-    for (int i = 0; i < argc; ++i) {
-        if (strcmp(argv[i], "-help") == 0) {
-            help();
-            return 0;
-        }
-    };
 
     int length = 8;
     int lat_little = 0;
 
     int num_of_pass = 1;
+    int flag_letter = 0;
+
+    for (int i = 0; i < argc; ++i) {
+        if (strcmp(argv[i], "-l") == 0) {
+            if ((i + 1) == argc) {
+                cerr << "Error len argument\n";
+                return 1;
+            }
+            length = atoi(argv[++i]);
+            if (length < 1) {
+                cerr << "Error len argument\n";
+                return 1;
+            } else {
+                if (strcmp(argv[i], "-help") == 0) {
+                    help();
+                    return 0;
+                }
+            }
+        }
+    }
+
+    if (flag_letter == 0){
+        lat_little = 1;
+    }
+
 
     for (int i = 0; i < num_of_pass; i++) {
         cout << password.RunGenerations(length, lat_little) << endl;
